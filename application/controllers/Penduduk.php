320a85bf596a745a35290df94c7fc771b31aca3f
zazaroyana123 <muhammadjajaroyana3@gmail.com>
2022-10-04 20:24:00 +0700

    membuat view profile

--- application/controllers/Penduduk.php
+++ application/controllers/Penduduk.php
@@ -12,14 +12,14 @@ class Penduduk extends CI_Controller
     {
         $data['judul'] = 'Pengaduan';
         $this->load->view('penduduk/header', $data);
-        $this->load->view('penduduk/index', $data);
+        $this->load->view('penduduk/pengaduan', $data);
         $this->load->view('penduduk/footer');
     }
     public function profile()
     {
         $data['judul'] = 'Profile';
         $this->load->view('penduduk/header', $data);
-        $this->load->view('penduduk/index', $data);
+        $this->load->view('penduduk/profile', $data);
         $this->load->view('penduduk/footer');
     }
 }
--- application/views/penduduk/profile.php
+++ application/views/penduduk/profile.php
@@ -1,1 +1,82 @@
-<h1 class="h3 mb-4 text-gray-800">Viwe Profile</h1>
+<div class="row mb-5">
+
+    <div class="col-lg-3">
+        <div class="card">
+            <div class="card-body">
+                <img class="card-img-top" src="<?= base_url('assets/img/profile/jaja.png'); ?>" alt="Card image cap">
+                <a href="#" class="btn btn-outline-primary mt-4 btn-block">Ganti Foto</a>
+                <a href="#" class="btn btn-outline-primary mt-4 btn-block">Edit Profile</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Data Pribadi</h5>
+                <div class="mt-2">
+                    <small class="mt-5">Nama Lengkap :</small>
+                    <div class="text-gray-800">
+                        Muhammad Jaja Royana
+                    </div>
+                    <hr>
+                </div>
+                <div class="mt-2">
+                    <small>Nomor Induk Kependudukan (NIK) :</small>
+                    <div class="text-gray-800">
+                        3215550403****99
+                    </div>
+                    <hr>
+                </div>
+                <div class="mt-2">
+                    <small class="mt-5">Tempat/Tanggal lahir :</small>
+                    <div class="text-gray-800">
+                        Subang, 4 Maret 2008
+                    </div>
+                    <hr>
+                </div>
+                <div class="mt-2">
+                    <small>Jenis Kelamin :</small>
+                    <div class="text-gray-800">
+                        Laki-laki
+                    </div>
+                    <hr>
+                </div>
+                <div class="mt-2">
+                    <small>Status :</small>
+                    <div class="text-gray-800">
+                        Belum kawin
+                    </div>
+                    <hr>
+                </div>
+                <div class="mt-2">
+                    <small>Alamat :</small>
+                    <div class="text-gray-800">
+                        Kp. Cinangka RT.10/03 Desa. Salamjaya
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card d-flex align-items-stretch">
+            <div class="card-body">
+                <h5 class="card-title">Kontak</h5>
+                <div class="mt-2">
+                    <small class="mt-5">No. Telephone :</small>
+                    <div class="text-gray-800">
+                        0813-8993-1321
+                    </div>
+                    <hr>
+                </div>
+                <div class="mt-2">
+                    <small>Alamat email :</small>
+                    <div class="text-gray-800">
+                        muhammadjajaroyana4@gmail.com
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
